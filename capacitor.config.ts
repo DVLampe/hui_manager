f38cc829e7ef5ebbf18988c67f8b2638cb4536b2
DVLampe <jenek_chan@mail.ru>
2025-11-09 03:00:55 +0700

    capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Hui Manager',
   webDir: 'out',
   server: {
-    url: 'http://192.168.1.4:3000',
+    url: 'http://10.0.2.2:3000',
     cleartext: true
   }
 };
